c81c51a1020858df0acb25b82786ad341924e642
Amit Kumar <amitsarajput@gmail.com>
2024-10-17 17:27:09 +0530

    send mail function check.15

--- laravel-core/app/Mail/GenricMail.php
+++ laravel-core/app/Mail/GenricMail.php
@@ -28,7 +28,7 @@ class GenricMail extends Mailable
     public function envelope(): Envelope 
     {
         return new Envelope(
-            subject: $this->mailData['customSubject']??'RadarTyres inquery from'.$this->mailData['name'],
+            subject: $this->mailData['customSubject']??'RadarTyres inquery from '.$this->mailData['name'],
         );
     }
 
