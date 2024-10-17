3e1e7e91cd517af9db5585453bfcdb568f255f9e
Amit Kumar <amitsarajput@gmail.com>
2024-10-17 15:53:28 +0530

    send mail function check.

--- laravel-core/resources/views/mails/genric-mail.blade.php
+++ laravel-core/resources/views/mails/genric-mail.blade.php
@@ -1,7 +1,9 @@
 <div>
     @foreach($mailData as $key=>$value)
-        {{ $key .':'.$value}} <br>
-        @if($key=='url_current')
+    
+        @if($key!='url_current')
+            <b>{{ ucfirst($key) }}</b>: {{ $value }}<br> 
+        @else
             <br><br><br>
             This message was sent from {{ $value }}
         @endif
