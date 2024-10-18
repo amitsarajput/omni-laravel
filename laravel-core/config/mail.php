ff49d6124788ce5f5f319999ba6c5efc399e5051
Amit Kumar <amitsarajput@gmail.com>
2024-10-18 22:23:27 +0530

    Updated why radar page text.

--- laravel-core/config/mail.php
+++ laravel-core/config/mail.php
@@ -44,14 +44,6 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout'    => null,
             'auth_mode'  => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            'verify_peer'       => false,
-            'stream'     => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer_name'  => false,
-                ],
-            ],
         ],
 
         'ses' => [
--- laravel-core/resources/views/pages/Unused/why-radar.blade-02.php
+++ laravel-core/resources/views/pages/Unused/why-radar.blade-02.php
@@ -44,4 +44,30 @@
         </div>
 
     </section><!-- #content end -->
+
+
+
+    <!-- Content
+    ============================================= -->
+    <section id="content" class="pb-4">
+        <div class="container">
+
+            <div class="grid">
+                <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+                    <ul class="pl-0">
+                        <li class="mb-xxs-1 mb-md-2">Tested against leading established brands, offering the same performance but without the hefty price tag.*</li>
+                        <li class="mb-xxs-1 mb-md-2">Designed in collaboration with renowned Italian design house GFG style, offering a perfect synergy of design and engineering.*</li>
+                        <li class="mb-xxs-1 mb-md-2">A one-stop-shop providing tyres for a wide range of applications and sizes, catering to diverse customer needs.</li>
+                        <li class="mb-xxs-1 mb-md-2">A warranty programme that provide peace of mind </li>
+                        <li class="mb-xxs-1 mb-md-2">Committed to giving back to society and sustainability, being the first manufactured carbon-neutral tyre brand.</li>
+                        <li class="mb-xxs-1 mb-md-2">Discover the unbeatable value of radar tyres with affordable premium performance.</li>
+                    </ul>
+                    <p class="text-sm mt-5">*Applies to select products only</p>
+                </div>
+                
+            </div>
+            
+
+        </div>
+    </section><!-- #content end -->
 </x-guest-layout>
