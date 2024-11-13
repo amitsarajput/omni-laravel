75b6c0c8d92f627e5318612c8edd7e1f864fa87a
Amit Kumar <amitsarajput@gmail.com>
2024-11-13 13:59:36 +0530

    Fix4

--- laravel-core/app/Providers/AppServiceProvider.php
+++ laravel-core/app/Providers/AppServiceProvider.php
@@ -11,9 +11,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if ($this->app->environment('production') || $this->app->environment('dev')) {
+            URL::forceScheme('https');
+        }
         //Public folder change
         $this->app->bind('path.public', function() {
-            return base_path('../../staging');
+            return base_path('../');
         });
     }
 
