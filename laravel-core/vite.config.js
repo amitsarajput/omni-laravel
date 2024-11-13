bcf231cf32a5740e7eb0febef3e4431bed5faa03
Amit Kumar <amitsarajput@gmail.com>
2024-11-13 16:01:04 +0530

    Fix11

--- laravel-core/vite.config.js
+++ laravel-core/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import fs from 'fs';  
-// const host = 'https://staging.radartyres.com'; 
+
 export default defineConfig({
     build: {
         outDir: '../build',
@@ -16,13 +15,6 @@ export default defineConfig({
             publicDirectory: "../",
             refresh: true,
         }),
-    ],    
-    // server: { 
-    //     host, 
-    //     hmr: { host }, 
-    //     https: { 
-    //         key: fs.readFileSync(`/path/to/${host}.key`), 
-    //         cert: fs.readFileSync(`/path/to/${host}.crt`), 
-    //     }, 
-    // }, 
+    ],
+    
 });
