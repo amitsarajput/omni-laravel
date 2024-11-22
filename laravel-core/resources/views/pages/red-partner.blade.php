147b92804f2ea04c7906af391165c4dec397fbd3
Amit Kumar <amitsarajput@gmail.com>
2024-11-22 17:33:18 +0530

    Red Partner Page fix

--- laravel-core/resources/views/pages/red-partner.blade.php
+++ laravel-core/resources/views/pages/red-partner.blade.php
@@ -8,24 +8,16 @@
     <section id="content" class="pb-5">
         <div class="container" >
             <div class="grid ">
-                <div class="col-md-12 col-bleed-y"><h2>RADAR RED PARTNER​</h2></div>
-            </div>
-            <div class="grid">
-                <div class="col-md-12">                    
+                <div class="col-md-12"><h3 class="no-top-margin dark-100">RADAR RED PARTNER​</h3>                                   
                     <p>The Radar RED(Radar Elite Dealer) Partner, is a community of trusted Radar Tyres dealers dedicated to delivering premium quality products and exceptional customer service. Our RED Partners are carefully selected for their commitment to excellence, product availability, and product knowledge, ensuring you receive the best possible tyre solutions for your needs.​</p>
                 </div>
             </div>
+            
             <div class="grid ">
-                <div class="col-md-12 col-bleed-y"><h3 class="no-top-margin dark-100 uppercase">Why Choose a Radar RED Partner?</h3></div>
                 
-            </div>
-            <div class="grid">
-                <div class="col-md-12">                    
-                    <p>The Radar RED(Radar Elite Dealer) Partner, is a community of trusted Radar Tyres dealers dedicated to delivering premium quality products and exceptional customer service. Our RED Partners are carefully selected for their commitment to excellence, product availability, and product knowledge, ensuring you receive the best possible tyre solutions for your needs.​</p>
-                </div>
-            </div>
-            <div class="grid">
                 <div class="col-md-12">
+                    <h4 class="no-top-margin dark-100 uppercase">Why Choose a Radar RED Partner?</h4>
+                    <p>The Radar RED(Radar Elite Dealer) Partner, is a community of trusted Radar Tyres dealers dedicated to delivering premium quality products and exceptional customer service. Our RED Partners are carefully selected for their commitment to excellence, product availability, and product knowledge, ensuring you receive the best possible tyre solutions for your needs.​</p>
                     <p>When you buy from a Radar RED Partner, you can enjoy:</p>
                     <ul>
                         <li>Elite Service: Benefit from personalized guidance and expert advice to find the perfect tyre for your vehicle.</li>
@@ -33,6 +25,10 @@
                         <li>Exclusive Customer Benefits: In certain markets, RED Partners may offer additional perks such as an additional road hazard replacement warranty, 30-day satisfaction promise, and mileage warranties. </li>
                         <li>Dedicated After-Sales Support: Seamless assistance for warranties, replacements, and tyre care.</li>
                     </ul>
+                </div>
+            </div>
+            <div class="grid">
+                <div class="col-md-12">
                     <h4 class="dark-100 uppercase">With a Radar RED Partner, you’re not just buying tyres—you’re investing in superior quality, trusted expertise, and unmatched customer care.</h4>
                 </div>
             </div>
