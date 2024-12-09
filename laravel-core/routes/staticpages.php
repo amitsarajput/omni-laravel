56f8ff2babb8ecd2b14dbe4a59602dfba0b81f10
Amit Kumar <amitsarajput@gmail.com>
2024-12-09 16:16:22 +0530

    Route without country update production

--- laravel-core/routes/staticpages.php
+++ laravel-core/routes/staticpages.php
@@ -42,49 +42,11 @@ Route::prefix('{country:slug}')->where(['country'=>'[a-zA-Z]{2,4}'])->name('page
 	Route::get('/testing', [StaticPagesController::class,'index'])->name('testing');
 	Route::get('/privacy-policy', [StaticPagesController::class,'index'])->name('privacy-policy');
 	Route::get('/red-partner', [StaticPagesController::class,'index'])->name('red-partner');
+});
+
+//Routes without country.
+Route::name('pages.')->group(function(){
 	Route::get('/environmental-responsibility', [StaticPagesController::class,'index'])->name('responsibility-environment');
 	Route::get('/social-responsibility', [StaticPagesController::class,'index'])->name('responsibility-social');
-	
-	//About Pages
-	// Route::name('about.')->group(function(){
-	// 	Route::get('/who-we-are', [StaticPagesController::class,'index'])->name('who-we-are');//
-	// 	Route::get('/mission-vision', [StaticPagesController::class,'index'])->name('mission-vision');
-	// 	Route::get('/our-values', [StaticPagesController::class,'index'])->name('our-values');
-	// 	Route::get('/ceo-messages', [StaticPagesController::class,'index'])->name('ceo-messages');
-	// 	Route::get('/leadership', [StaticPagesController::class,'index'])->name('leadership');
-	// 	Route::get('/awards', [StaticPagesController::class,'index'])->name('awards');
-	// 	Route::get('/our-story', [StaticPagesController::class,'index'])->name('our-story');
-	// });
-	
-	//Warranty Pages
-	// Route::name('warranty.')->group(function(){
-	// 	Route::get('radar-us/limited-warranty', [StaticPagesController::class,'index'])->name('radarus');
-	// 	Route::get('/radar/limited-warranty', [StaticPagesController::class,'index'])->name('radar');
-	// 	Route::get('/patriot-us/limited-warranty', [StaticPagesController::class,'index'])->name('patriotus');
-	// 	Route::get('/patriot/limited-warranty', [StaticPagesController::class,'index'])->name('patriot');
-	// 	Route::get('/american-tourer-us/limited-warranty', [StaticPagesController::class,'index'])->name('americantourerus');
-	// 	Route::get('/american-tourer/limited-warranty', [StaticPagesController::class,'index'])->name('americantourer');
-	// });
-	//Program Pages
-	// Route::name('dealerlocator.')->group(function(){
-	// 	Route::get('/north-america-dealer-locator', [StaticPagesController::class, 'index'])->name('northamerica');
-	// 	Route::get('/uk-europe-dealer-locator', [StaticPagesController::Class, 'index'])->name('uk');
-	// });
-	//Program Pages
-	// Route::name('program.')->group(function(){
-	// 	Route::get('/golf', [GolfTournamentController::class, 'index'])->name('golf');
-	// 	Route::get('/motorsport', [MotorsportController::Class, 'index'])->name('motorsport');
-	// });
-	//Responsibility Pages
-	// Route::name('responsibility.')->group(function(){
-	// 	Route::get('/social-responsibility', [StaticPagesController::class, 'index'])->name('social');
-	// 	Route::get('/environmental-responsibility', [StaticPagesController::class, 'index'])->name('environmental');
-	// 	Route::get('/climate-change', [ClimateNewsController::class, 'index'])->name('climatechange');
-	// });
-	//MediaCenter Pages
-	// Route::name('mediacentre.')->group(function(){
-	// 	Route::get('/media-coverage', [MediaController::class, 'coverage'])->name('mediacoverage');
-	// 	Route::get('/press-releases', [MediaController::class, 'press'])->name('pressreleases');
-	// });
 });
 
